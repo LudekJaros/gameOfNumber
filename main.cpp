cd60215f649d52f65cb4b22696669dc6ea60d97d
LudekJaros <it-ludekjaros@stredniskola.net>
2024-06-07 08:31:27 +0200

    Inicializován generátor náhodných čísel

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include "iostream"
 #include "color.h"
+#include "ctime"
 
 /*Guessing a secret number*/
 using namespace std;
 int main(int argc, char *argv[]){
 	system("chcp 65001>nul");
+	srand(time(0));
 	int secretNumber;
 	int playersNumber;
 	secretNumber = rand()%5+1; // random from 1 to 5
